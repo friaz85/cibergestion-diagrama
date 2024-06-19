e46060d9508f9fdd219adcd85b72581763fa517b
Alejandro Fernando Frias Torres <alejandro.frias@itecsi.mx>
2024-06-18 22:17:24 -0600

    Menú contextal

--- diagramas/src/app/app.component.ts
+++ diagramas/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { FALSE } from './../../node_modules/sass/types/legacy/function.d';
 /* eslint-disable */
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BasicShapeModel, ConnectorModel, DecoratorModel, DiagramComponent, HorizontalAlignment, IClickEventArgs, ImageElement, MarginModel, NodeConstraints, NodeModel, PointModel, PointPortModel, PortVisibility, ShapeStyleModel, SnapConstraints, SnapSettingsModel, StrokeStyleModel, VerticalAlignment } from '@syncfusion/ej2-angular-diagrams';
+import {
+  BasicShapeModel, ConnectorModel, ContextMenuSettingsModel,
+  DecoratorModel, DiagramBeforeMenuOpenEventArgs, DiagramComponent,
+  HorizontalAlignment, IClickEventArgs, ImageElement, MarginModel, NodeConstraints,
+  NodeModel, PointModel, PointPortModel, PortVisibility, ShapeStyleModel, SnapConstraints,
+  SnapSettingsModel, StrokeStyleModel, VerticalAlignment
+} from '@syncfusion/ej2-angular-diagrams';
+import { MenuEventArgs } from '@syncfusion/ej2-navigations';
+import { createElement } from "@syncfusion/ej2-base";
+
+
 
 @Component({
   selector: 'app-root',
@@ -30,6 +40,7 @@ export class AppComponent {
   public nodes: any = [];
   public sourcePoint?: PointModel;
   public targetPoint?: PointModel;
+  public contextMenuSettings?: ContextMenuSettingsModel;
   selectedOption = 0;
   offsetX = 0;
   public snapSettings: SnapSettingsModel = {
@@ -126,31 +137,55 @@ export class AppComponent {
       visibility: PortVisibility.Hidden
     }];
 
-    public port10: PointPortModel[] = [
-      {
-        id: 'port10',
-        offset: {
-          x: 0,
-          y: 0.5
-        },
-        visibility: PortVisibility.Hidden
+  public port10: PointPortModel[] = [
+    {
+      id: 'port10',
+      offset: {
+        x: 0,
+        y: 0.5
       },
-      {
-        id: 'port11',
-        offset: {
-          x: 0.5,
-          y: 0
-        },
-        visibility: PortVisibility.Hidden
-      }];
+      visibility: PortVisibility.Hidden
+    },
+    {
+      id: 'port11',
+      offset: {
+        x: 0.5,
+        y: 0
+      },
+      visibility: PortVisibility.Hidden
+    }];
 
-      constructor (
-        private modalService: NgbModal)
-      {
+  constructor(
+    private modalService: NgbModal,
+    private ref: ChangeDetectorRef) {
 
-      }
+  }
 
   ngOnInit(): void {
+
+    let $this = this as any;
+    this.contextMenuSettings = {
+      //Enables the context menu (Lista de acciones del menú desplegable)
+      show: true,
+      items: [
+        { id: 'node1', text: 'Menu 1', iconCss: '#node1' },
+        { id: 'node1', text: 'Menu 2', iconCss: '#node1' },
+        { id: 'node2', text: 'Menu 3', iconCss: '#node2' },
+        { id: 'node3', text: 'Menu 4', iconCss: '#node3' },
+        { id: 'node3', text: 'Menu 5', iconCss: '#node3' },
+        { id: 'node4', text: 'Menu 6', iconCss: '#node4' },
+        { id: 'node5', text: 'Menu 7', iconCss: '#node5' },
+        { id: 'node6', text: 'Menu 8', iconCss: '#node6' },
+        { id: 'node6', text: 'Menu 9', iconCss: '#node6' },
+        { id: 'node6', text: 'Menu 10', iconCss: '#node6' },
+        { id: 'node7', text: 'Menu 11', iconCss: '#node7' },
+        { id: 'node8', text: 'Menu 12', iconCss: '#node8' },
+        { id: 'node9', text: 'Menu 14', iconCss: '#node9' },
+
+      ],
+      // Hides the default context menu items
+      showCustomMenuOnly: true,
+    } as ContextMenuSettingsModel;
 
     this.shape1 = { type: 'Image', source: '../assets/icons/1_notarial_1.svg', scale: 'None' } as unknown as ImageElement;
     this.shape2 = { type: 'Image', source: '../assets/icons/1_ingre_1.svg', scale: 'None' } as unknown as ImageElement;
@@ -162,13 +197,17 @@ export class AppComponent {
     this.shape8 = { type: 'Image', source: '../assets/icons/1_mesa_1.svg', scale: 'None' } as unknown as ImageElement;
     this.shape9 = { type: 'Image', source: '../assets/icons/1_progra_1.svg', scale: 'None' } as unknown as ImageElement;
 
+    // Configuración de las etiqueta de cada nodo
     this.margin = { top: 10 }
     this.horizontalAlignment = 'Center'
     this.verticalAlignment = 'Top'
     this.offset = { x: 0.5, y: 1 }
-    // this.nodeConstraints = NodeConstraints.Default & ~NodeConstraints.Rotate & ~NodeConstraints.Drag &
-    //   ~NodeConstraints.Resize & ~NodeConstraints.Delete & ~NodeConstraints.Tooltip;
 
+    // Habilitala/Deshabilita las funciones de los nodos
+    this.nodeConstraints = NodeConstraints.Default & ~NodeConstraints.Rotate & ~NodeConstraints.Drag &
+      ~NodeConstraints.Resize & ~NodeConstraints.Delete & ~NodeConstraints.Tooltip;
+
+    // Oculto leyenda de licencia
     let el = document.getElementsByTagName('div');
     for (let i = 0; i < el.length; i++) {
       if (el[i].innerText == 'This application was built using a trial version of Syncfusion Essential Studio. To remove the license validation message permanently, a valid license key must be included. Claim your free account') {
@@ -180,34 +219,41 @@ export class AppComponent {
 
   public click(args: IClickEventArgs): void {
     // Obtains the mouse button clicked
-    console.log(args.actualObject.nodes ? args.actualObject.nodes[0].id : '');
-    let id = args.actualObject.nodes ? args.actualObject.nodes[0].id : '';
+    // console.log(args.actualObject.nodes ? args.actualObject.nodes[0].id : '');
+    // let id = args.actualObject.nodes ? args.actualObject.nodes[0].id : '';
 
-    this.modalService.open(this.modalContent, { ariaLabelledBy: 'modal-basic-title', centered: true, backdrop: false }).result.then(
-			(result) => {
-				console.log(result);
-        // change image from node
-          let i = this.diagram.nodes.findIndex((node: any) => node.id == id);
-          let img = '';
-          this.diagram.nodes[i].shape = { type: 'Image', source: '../assets/icons/' + id + '-' + result + '.svg'};
-          (this.diagram as DiagramComponent).dataBind();
-			},
-			(reason) => {
-				// this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
-		);
+    // this.modalService.open(this.modalContent, { ariaLabelledBy: 'modal-basic-title', centered: true, backdrop: false }).result.then(
+    // 	(result) => {
+    // 		console.log(result);
+    //     // change image from node
+    //       let i = this.diagram.nodes.findIndex((node: any) => node.id == id);
+    //       let img = '';
+    //       this.diagram.nodes[i].shape = { type: 'Image', source: '../assets/icons/' + id + '-' + result + '.svg'};
+    //       (this.diagram as DiagramComponent).dataBind();
+    // 	},
+    // 	(reason) => {
+    // 	},
+    // );
 
   }
 
   public getConnectorDefaults(obj: ConnectorModel): void {
-    // Set Source Padding value
-    // obj.sourcePadding = 20
-    // Set Target Padding value
-    // obj.targetPadding = 20
+    // Configuración de las lineas de conexión de los nodos
+    obj.style = {
+      strokeColor: '#6BA5D7',
+      fill: '#6BA5D7',
+      strokeWidth: 2
+    }
+    obj.targetDecorator = {
+      style: {
+        fill: '#6BA5D7',
+        strokeColor: '#6BA5D7'
+      }
+    }
   }
 
   public created(args: Object): void {
-    this.offsetX = this.diagram?.scrollSettings.viewPortWidth / 2;
+    // Configuración de los nodos después de la creación
     this.diagram?.fitToPage();
     for (let i = 0; i < this.diagram?.connectors.length; i++) {
       ((this.diagram as DiagramComponent).connectors[i].style as StrokeStyleModel).strokeColor = '#a4bec9';
@@ -220,11 +266,10 @@ export class AppComponent {
 
       (this.diagram as DiagramComponent).dataBind();
     }
-
-
   }
 
   public getNodeDefaults(node: NodeModel | any): NodeModel {
+    // Configuración de los nodos
     node.height = 120;
     node.width = 150;
     // node.HorizontalAlignment = 'Center';
@@ -232,4 +277,34 @@ export class AppComponent {
     ((node as NodeModel).style as ShapeStyleModel).strokeColor = 'none';
     return node;
   }
+
+  public contextMenuOpen(args: DiagramBeforeMenuOpenEventArgs): void {
+    // Menú contextual
+    console.log(args);
+
+    for (let i = 0; i < args.element.children.length; i++) {
+      if (this.diagram.selectedItems.nodes.length > 0) {
+        if (args.element.children[i].id == this.diagram.selectedItems.nodes[0].id) {
+          args.element.children[i].className = 'd-block';
+        } else {
+          args.element.children[i].className = 'd-none';
+        }
+      } else {
+        args.element.children[i].className = 'd-none';
+      }
+    }
+
+  }
+
+  public contextMenuClick(args: MenuEventArgs): void {
+    // Acciones del menú contextual
+    console.log(this.diagram.selectedItems.nodes[0].id);
+    if (args.item.id === 'delete') {
+      // if (((this.diagram as any).selectedItems.nodes.length + (this.diagram as any).selectedItems.connectors.length) > 0) {
+      //   (this.diagram as any).cut();
+      // }
+    }
+  }
+
+
 }
